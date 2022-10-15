cf84bc05e4cb9beec976e9fad975e7aab1b8d07c
Viktor <viktor.belebezyev@yandex.ru>
2022-10-15 23:22:57 +0300

    add solution algorithm

--- task1/Program.cs
+++ task1/Program.cs
@@ -2,3 +2,16 @@
 
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+Console.Write("Введите число N: ");
+int maxValue = int.Parse(Console.ReadLine()!);
+
+
+Console.WriteLine($"Последовательные числа от {maxValue} до 1 --> {Recurs(maxValue)}");
+
+string Recurs(int maxValue)
+{
+    if (maxValue == 1) return maxValue.ToString();
+    return (maxValue + " " + Recurs(maxValue - 1));
+}
