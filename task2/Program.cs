64c3241ac680cfc698bb1396c2bc9c17ff401471
Viktor <viktor.belebezyev@yandex.ru>
2022-10-15 23:31:56 +0300

    add solution algorithm

--- task2/Program.cs
+++ task2/Program.cs
@@ -2,3 +2,17 @@
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int min = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N: ");
+int max = int.Parse(Console.ReadLine()!);
+
+
+Console.WriteLine($"Сумма элементов в промежутке от {min} до {max} --> {SumRecurs(min, max)}");
+
+int SumRecurs(int min, int max)
+{
+    if (min == max) return max;
+    return (min + SumRecurs(min + 1, max));
+}
